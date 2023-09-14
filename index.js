a0a9dd4668358a07a35713f421f2055c2b586c06
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-09-14 16:56:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+
 function addingEventListener() {
+    // Inside this function, we select the button element
+    const button = document.querySelector("input[type='button']");
+    
+    // Define the clickAlert function
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    
+    // Add the click event listener to the button
+    button.addEventListener('click', clickAlert);
 }
+
+// To set up the event listener, call the addingEventListener function
+addingEventListener();
